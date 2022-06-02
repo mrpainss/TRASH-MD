cf27667dad7d10c9316f8e1564b22cc6bd1174a5
mrpainss <105528316+mrpainss@users.noreply.github.com>
2022-06-02 21:10:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.owner = ['6281220670449','0']
+global.owner = ['6281239413351','0']
 global.packname = 'Bot Whatsapp'
-global.author = 'TRASH'
+global.author = 'Dimassh'
 global.sessionName = 'data'
 global.prefa = ['','!','.','#']
-global.ownername = 'KurrXd'
+global.ownername = 'DimasSh'
 global.sp = '>'
 global.mess = {
     admin: 'Fitur Khusus Admin Group!',
